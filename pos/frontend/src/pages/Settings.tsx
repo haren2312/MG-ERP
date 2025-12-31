e1ba1d331aa4bb22c7ce4b335b9c4dd55ee88eaf
khaled Elgohary <k.elgohare@gmail.com>
2025-12-31 17:03:20 +0100

    removing the vars

--- pos/frontend/src/pages/Settings.tsx
+++ pos/frontend/src/pages/Settings.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
-import { testESCPOSPrinter, ESCPOSPrinter, createDummyReceiptData } from '../utils/escPosTest';
+import { ESCPOSPrinter, createDummyReceiptData } from '../utils/escPosTest';
 
 interface POSSettings {
   id: number;
